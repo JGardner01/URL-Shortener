ab80feda876d7f12fb1f771f96b23c202d5062af
JGardner01 <jamesjmg@live.co.uk>
2024-08-09 22:45:28 +0100

    In routes.py created a new route, redirect_url. Allows for redirecting users to the original url and returns 404 error message if not found. The index post route now checks to see if the user's inputted url starts with http or https and adds it if not to ensure the expected redirect.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, abort
 from app import app, urls
 from .shortener import generate_short_url_code
 
@@ -7,13 +7,24 @@ from .shortener import generate_short_url_code
 def index():
     if request.method == "POST":
         url = request.form.get("url")
+
+        if not (url.startswith("http://") or url.startswith(("https://"))):
+            url = "http://" + url
+
         short_url_code = generate_short_url_code()
 
         urls.insert_one({
-            "url": url,
+            "original_url": url,
             "short_url_code": short_url_code
         })
 
         return request.host_url + short_url_code
     return render_template("index.html")
 
+@app.route("/<short_url_code>")
+def redirect_url(short_url_code):
+    url = urls.find_one({"short_url_code" : short_url_code});
+    if url:
+        return redirect(url["original_url"])
+    else:
+        return abort(404)   #temporary error message for short url not found
