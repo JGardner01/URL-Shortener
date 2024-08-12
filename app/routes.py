eb4416ebaf0a3025af6fcf6cff8837bebdffc085
JGardner01 <jamesjmg@live.co.uk>
2024-08-12 19:17:52 +0100

    Added front end validation for entering the URL (changed form input type to url rather than text(may change to a different solution for usability in future)). Added backend validation for url entry - installed validators package and used in the route index, returning error if not valid.

--- app/routes.py
+++ app/routes.py
@@ -2,23 +2,31 @@ from flask import render_template, request, redirect, abort
 from app import app, urls
 from .shortener import generate_short_url_code
 
+import validators
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        url = request.form.get("url")
+        url = request.form.get("url")   #get string from form
 
+        #check/validate here
         if not (url.startswith("http://") or url.startswith(("https://"))):
             url = "http://" + url
 
+        if not (validators.url(url)):
+            return "URL entry error"    #temporary output
+
+        #generate code
         short_url_code = generate_short_url_code()
 
+        #insert to database
         urls.insert_one({
             "original_url": url,
             "short_url_code": short_url_code
         })
 
-        return request.host_url + short_url_code
+        return request.host_url + short_url_code    #domain + code (temporary output)
     return render_template("index.html")
 
 @app.route("/<short_url_code>")
